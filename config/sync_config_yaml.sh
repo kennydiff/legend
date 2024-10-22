733bc4e9ac99f4865f7a5cad7aa6f8a2781c15de
KennyDiff <19239945+kennydiff@users.noreply.github.com>
2024-10-22 14:47:16 +0800

    1

---
+++ config/sync_config_yaml.sh
@@ -0,0 +1,3 @@
+# K_24A21 不要修改代码库这边的 config.yaml, 无法保存，会被覆盖，只做备份
+rsync -a /Users/ken/Library/Containers/com.example.legend/Data/config.yaml /Users/ken/Sync/ws/0.Flutter/legend/config/config.yaml
+# cp /Users/ken/Library/Containers/com.example.legend/Data/config.yaml /Users/ken/Sync/ws/0.Flutter/legend/config/config.yaml
